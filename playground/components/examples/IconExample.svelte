f3b269eea3581ac13dfec56ab33b280c59db7cdd
Ivan Chen <tachen2@cisco.com>
2021-02-12 23:26:30 +0800

    feat: Icon example, with heroicons refer,

--- playground/components/examples/IconExample.svelte
+++ playground/components/examples/IconExample.svelte
@@ -2,6 +2,8 @@
     import Icon from '../../../src/components/Icon'
 
     export const code = `
+Check https://heroicons.com/
+
 <Icon icon="outline--sun" class="tw-w-8 tw-h-8 tw-text-pink-40"></Icon>
 <Icon icon="solid--sun" class="tw-text-theme-60"></Icon>
 <Icon icon="outline--moon" style="width: 8px; height: 20px"></Icon>
